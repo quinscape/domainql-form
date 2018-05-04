a8924a0441559ca023eb9fc48ceff6911ed2c6b7
Sven Helmberger <fforw@gmx.de>
2018-05-04 04:11:45 +0200

    Add simple tests
    Add test for input value conversion in both directions and test some
    field validations
    
    The schema.json is an GraphQL introspection result from the current test
    project.
    
    address.json a "DomainTypeInput" value, the address domainql type of that
    example.
    
    address-values.json is the same in it's internal formik form. All fields
    are strings except for boolean which are boolean

---
+++ test/input-schema.js
@@ -0,0 +1,124 @@
+import rawSchema from "./schema.json"
+import InputSchema from "../src/InputSchema";
+
+import assert from "power-assert"
+
+import ADDRESS_OBJECT_TYPED from "./address.json"
+import ADDRESS_OBJECT_VALUES from "./address-values.json"
+
+describe("InputSchema", function () {
+    it("converts complex values to formik values", function () {
+
+        const inputSchema = new InputSchema(rawSchema);
+
+        const converted = inputSchema.toValues("DomainTypeInput", ADDRESS_OBJECT_TYPED);
+
+        //console.log(JSON.stringify(converted));
+
+        assert( converted.name === "Address");
+        assert( converted.fields.length === 6);
+        assert( converted.fields[0].name === "id");
+        assert( converted.primaryKey.fields.length === 1);
+        assert( converted.primaryKey.fields[0] === "id");
+
+        // following formik, bools are real bools :\
+        assert( converted.fields[0].required === true);
+        assert( converted.fields[0].unique === true);
+        // everything else is string
+        assert( converted.fields[0].maxLength === "36");
+
+        assert( converted.description === "");
+    });
+
+    it("does type-based formik values validation", function () {
+
+        const inputSchema = new InputSchema(rawSchema);
+
+        {
+
+            const errors = inputSchema.validate("DomainTypeInput", {});
+
+            assert(Object.keys(errors).length === 5);
+
+            assert( errors.name === "$FIELD required");
+            assert( errors.fields === "$FIELD required");
+            assert( errors.primaryKey === "$FIELD required");
+            assert( errors.foreignKeys === "$FIELD required");
+            assert( errors.uniqueConstraints === "$FIELD required");
+
+        }
+        {
+
+            const errors = inputSchema.validate("DomainTypeInput", {
+                name: "Test",
+                fields: [],
+                primaryKey: {
+                    fields: ["id"]
+                },
+                uniqueConstraints: [],
+                foreignKeys: []
+            });
+
+            assert(Object.keys(errors).length === 4);
+
+            assert( errors.fields.length === 0);
+            assert( Object.keys(errors.primaryKey).length === 0);
+            assert( errors.foreignKeys.length === 0);
+            assert( errors.uniqueConstraints.length === 0);
+
+        }
+
+        {
+
+            const errors = inputSchema.validate("DomainTypeInput", {
+                name: "Test",
+                fields: [{
+                    name : "f1",
+                    type: "PLAINTEXT",
+                    required: false,
+                    maxLength: "3a"
+                }],
+                primaryKey: {
+                    fields: ["id"]
+                },
+                uniqueConstraints: [],
+                foreignKeys: []
+            });
+
+            console.log(errors);
+
+            assert(Object.keys(errors).length === 4);
+
+            assert( errors.fields.length === 1);
+            assert( errors.fields[0].maxLength === "Invalid Integer");
+            assert( Object.keys(errors.primaryKey).length === 0);
+            assert( errors.foreignKeys.length === 0);
+            assert( errors.uniqueConstraints.length === 0);
+
+        }
+
+
+    });
+    it("converts formik values back to typed values", function () {
+
+        const inputSchema = new InputSchema(rawSchema);
+
+        const converted = inputSchema.fromValues("DomainTypeInput", ADDRESS_OBJECT_VALUES);
+
+        //console.log(converted);
+
+        assert( converted.name === "Address");
+        assert( converted.description === "");
+        assert( converted.fields.length === 6);
+        assert( converted.fields[0].name === "id");
+        assert( converted.primaryKey.fields.length === 1);
+        assert( converted.primaryKey.fields[0] === "id");
+
+        // following formik, bools are real bools :\
+        assert( converted.fields[0].required === true);
+        assert( converted.fields[0].unique === true);
+        // everything else is string
+        assert( converted.fields[0].maxLength === 36);
+
+    });
+});
