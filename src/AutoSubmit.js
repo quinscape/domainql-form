bacba32423f7ce028c6695d096ec15ac43781f97
Sven Helmberger <sven.helmberger@quinscape.de>
2018-07-03 14:26:17 +0200

    Implement <AutoSubmit/>
    Implements a signal component <AutoSubmit/> that can be dropped into a
    domainql-form.
    
    It renders nothing but listens for form updates. If the form has no
    errors and the last form content is not deep equal to the next form
    content, the component triggers a debounced submit for the form.
    
    The debounce timeout can be set by calling
         <AutoSubmit timeout={ 200}/>
    
    The default is 300ms.

---
+++ src/AutoSubmit.js
@@ -0,0 +1,74 @@
+import React from "react"
+import PropTypes from "prop-types"
+import withFormConfig from "./withFormConfig";
+
+import debounce from "lodash.debounce"
+import isEqual from "lodash.isequal"
+
+
+
+class AutoSubmit extends React.Component {
+
+    static propTypes = {
+        /**
+         * Debounce timeout in milliseconds.
+         */
+        timeout: PropTypes.number
+    };
+
+    static defaultProps = {
+        timeout: 300
+    };
+
+    static getDerivedStateFromProps(nextProps, prevState)
+    {
+        const { formConfig } = nextProps;
+
+        //console.log("AutoSubmit.getDerivedStateFromProps", nextProps, prevState);
+
+        if (!prevState.formConfig)
+        {
+            //console.log("NO prevState or config, set ", formConfig);
+
+            return {
+                formConfig
+            };
+        }
+
+        if (prevState.formConfig !== formConfig)
+        {
+            const { formConfig : oldConfig, instance } = prevState;
+
+            const { values : prevValues } = oldConfig.formikProps;
+            const { values } = formConfig.formikProps;
+
+            if (!isEqual(prevValues, values))           
+            {
+                //console.log("triggerSubmit");
+                instance.triggerSubmit();
+            }
+
+            //console.log("Update ", prevValues, values, formConfig);
+
+            return {
+                formConfig
+            }
+        }
+
+        return null;
+    }
+
+    state = {
+        instance: this
+    };
+
+    triggerSubmit = debounce( () => this.props.formConfig.formikProps.submitForm(), this.props.timeout);
+
+    render()
+    {
+        // render nothing
+        return false;
+    }
+}
+
+export default withFormConfig(AutoSubmit)
