52f9e38ba99a8db19fe68dd7187d356df1b95d1e
Sven Helmberger <fforw@gmx.de>
2018-07-06 12:24:22 +0200

    Icon: Add brand prop
    Add a brand prop that makes Icon use the brand base class "fab" instead
    of "fas".

--- src/util/Icon.js
+++ src/util/Icon.js
@@ -1,4 +1,5 @@
 import React from "react"
+import cx from "classnames"
 import PropTypes from "prop-types"
 
 
@@ -9,18 +10,21 @@ class Icon extends React.Component {
 
     static propTypes = {
         className: PropTypes.string.isRequired,
-        title: PropTypes.string
+        title: PropTypes.string,
+        brand: PropTypes.bool
     };
 
     render()
     {
 
-        const { className } = this.props;
+        const { className, brand , ... rest} = this.props;
 
         return (
             <i
-                {... this.props}
-                className={"fas " + className}
+                { ... rest }
+                className={
+                    cx( brand ? "fab" : "fas", className)
+                }
             />
         );
     }
