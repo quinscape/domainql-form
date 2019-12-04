a82c7b9c848ed39ff65df58c1fb5b50351be7d77
Sven Helmberger <sven.helmberger@quinscape.de>
2019-12-04 15:35:11 +0100

    Refactor Icon
    The <Icon/> component was both too limited and too much geared to our
    own current usage for LIS-A.
    
    This commit gets rid of the `brand` prop and replaces it with a general
    `family` prop that takes a symbolic name and converts it to the
    corresponding fontawesome class.
    
    We export the valid values as static members of Icon
    
     * Icon.SOLID
     * Icon.REGULAR
     * Icon.LIGHT
     * Icon.DUOTONE
     * Icon.BRAND
    
    These are all the families fontawesome currently has. Note that you need
    to provide the corresponding stylesheets in your base template / CSS.
    
    So a brand icon is now defined as
    
        <Icon
            className="quinscape"
            family={ Icon.BRAND }
        />

--- src/util/Icon.js
+++ src/util/Icon.js
@@ -4,27 +4,63 @@ import PropTypes from "prop-types"
 
 
 /**
+ * Bootstrap 4 classes for the symbolic icon family names.
+ *
+ * @type {{REGULAR: string, DUOTONE: string, LIGHT: string, SOLID: string, BRAND: string}}
+ */
+const familyClasses = {
+    SOLID : "fas",
+    REGULAR : "far",
+    LIGHT : "fal",
+    DUOTONE : "fad",
+    BRAND : "fab"
+};
+
+/**
  * Simple FontAwesome Icon component
  */
 const Icon = props =>  {
-    
-    const { className, brand, tooltip, ... rest} = props;
+
+    const { className, family = Icon.SOLID, ... rest } = props;
 
     return (
         <i
             { ... rest }
-            title={ tooltip }
             className={
-                cx( brand ? "fab" : "fas", className)
+                cx(
+                    familyClasses[family],
+                    className
+                )
             }
         />
     );
 };
 
+Icon.SOLID = "SOLID";
+Icon.REGULAR = "REGULAR";
+Icon.LIGHT = "LIGHT";
+Icon.DUOTONE = "DUOTONE";
+Icon.BRAND = "BRAND";
+
 Icon.propTypes = {
+    /**
+     * Fontawesome icon as class name
+     */
     className: PropTypes.string.isRequired,
-    tooltip: PropTypes.string,
-    brand: PropTypes.bool
+    /**
+     * Optional tooltip / title
+     */
+    title: PropTypes.string,
+    /**
+     * true if icon is a brand icon
+     */
+    family: PropTypes.oneOf([
+        "SOLID",
+        "REGULAR",
+        "LIGHT",
+        "DUOTONE",
+        "BRAND"
+    ])
 };
 
 export default Icon
